4f073ed38314ad728fb97e82262b97975968e4c8
huuduynvc <nguyenhuuduynvc@gmail.com>
2021-07-26 21:58:38 +0700

    fix xgboost

--- models/xgb.py
+++ models/xgb.py
@@ -13,13 +13,13 @@ class Xgb:
         self.n_days = n_days
 
         self.model = xgb.XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-             colsample_bynode=1, colsample_bytree=1, gamma=0, gpu_id=-1,
+             colsample_bynode=1, colsample_bytree=1, gamma=0,
              importance_type='gain', interaction_constraints='',
              learning_rate=0.300000012, max_delta_step=0, max_depth=6,
-             min_child_weight=1, monotone_constraints='()',
+             min_child_weight=1,
              n_estimators=100, n_jobs=0, num_parallel_tree=1, random_state=0,
              reg_alpha=0, reg_lambda=1, scale_pos_weight=1, subsample=1,
-             tree_method='exact', validate_parameters=1, verbosity=None,
+             tree_method='exact', validate_parameters=1,
             objective='reg:squarederror'
         )
         if os.path.isfile(path):
